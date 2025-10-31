e1cfaa6d72afbc069be20ae13795a3741c177cd6
Усова Софья <son.usowa@yandex.ru>
2025-10-31 21:30:46 +0500

    Описание изменений конкретного файла

--- Lab7_6.py
+++
@@ -1,7 +0,0 @@
-with open('input.txt', 'a+') as f:
-    f.write('\nНе уходи, постой, фанатка!')
-    f.seek(0)
-    result = f.readlines()
-    print(result)
-
-
--- Lab7_7.py
+++
@@ -1,5 +0,0 @@
-lines = ['лучшая!', 'гениальная', 'красивая']
-with open('input.txt', 'w') as f:
-    for line in lines:
-        f.write('\nЯ-'+line)
-    print('Done!')
